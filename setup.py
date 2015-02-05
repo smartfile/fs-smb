258500b799974d85c360d8737fed9e2beb01d65b
Travis Cunningham <travcunn@umail.iu.edu>
2015-02-05 10:43:56 -0500

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), readme_file)) as f:
 
 setup(
     name='smbfs',
-    version='0.1',
+    version='0.2',
     packages=['smbfs'],
     description='A PyFilesystem backend for SMB shares.',
     long_description=long_description,
